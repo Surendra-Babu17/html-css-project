59f1d23a32b195ccdd9240ffffb06e5ed2378b19
SURENDRA BABU <surendrasvsk@gmail.com>
2025-07-25 17:11:24 +0530

    food project

---
+++ index.js
@@ -0,0 +1,36 @@
+// console.log(+"1"+true);
+// console.log(undefined === undefined);
+// console.log(undefined == null);
+// console.log(2 + undefined);
+// console.log(typeof 2);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof 1n + 2n);
+// console.log(typeof 1 / 1n);
+// console.log(1 + "1" - 1)
+// console.log("10" - "5" + "5");
+// console.log("10" - 5 + 10);
+// console.log(+""); 
+
+ 
+var score =50;
+if (score<=45){
+    console.log("You are fail");
+} 
+else if (score > 45 && score <= 60) {
+    console.log("You are pass");
+} 
+else if (score > 60 && score <= 80) {
+    console.log("You are good");
+} 
+else if (score > 80 && score <= 100) {
+    console.log("You are excellent");
+}
+else {
+    console.log("Invalid score");
+}
+
+
+
+
+
